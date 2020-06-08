b77a498a100f3e4b87b9053f32f3574e01247211
Mengxi Zhang <zmx94824@gmail.com>
2020-06-07 20:32:42 -0700

    generate react app by reactjs-express-generate

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {};
+
+config.admin={};
+
+config.app={};
+
+config.admin.name="Mengxi";
+config.admin.email="";
+config.app.name="TravelBuddyUI";
+config.app.url="";
+
+module.exports=config
